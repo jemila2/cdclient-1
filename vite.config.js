f072e79d89d380bf868202ada54d2d48dd474edc
jemila <abubakarjemiletu08@gmail.com>
2025-09-06 09:46:43 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/cdclient-1/', // Make sure this matches your GitHub repo name
   plugins: [
     react(),
+    tailwindcss(),
   ],
   css: {
     postcss: './postcss.config.js',
