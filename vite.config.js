f53f9a72a4602240872e91be94ddc7d3b53ffb3b
jemila <abubakarjemiletu08@gmail.com>
2025-09-06 09:12:41 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cdclient-1/', 
+  base: '/cdclient-1/', // Make sure this matches your GitHub repo name
   plugins: [
     react(),
   ],
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5173,
   },
   build: {
-    outDir: 'public',
+    outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     sourcemap: false,
