c4897990a356e98e7428110af0171cb2313b916e
jemila <abubakarjemiletu08@gmail.com>
2025-09-06 08:55:57 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/cdclient-1/', 
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  build: {
+    outDir: 'public',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'assets/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
+});
