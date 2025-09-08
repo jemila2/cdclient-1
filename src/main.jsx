9ac0a6f061f35f4ece53f585e760ebd833a24c24
jemila <abubakarjemiletu08@gmail.com>
2025-09-08 17:13:06 +0100

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-
-// if (typeof window.VITE_API_BASE_URL === 'undefined') {
-//   window.VITE_API_BASE_URL = 'https://backend-21-2fu1.onrender.com';
-// }
-// if (typeof window.API_BASE_URL === 'undefined') {
-//   window.API_BASE_URL = 'https://backend-21-2fu1.onrender.com';
-
-
-// Your existing React imports
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-
-);
+import React from 'react' 
+import ReactDOM from 'react-dom/client' 
+import App from './App' 
+import './index.css' 
+ 
+ReactDOM.createRoot(document.getElementById('root')).render( 
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode> 
+) 
